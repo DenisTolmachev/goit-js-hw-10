cc8ad6b50d7d2851798941e40271e2e6871e98ed
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-13 22:05:01 +0300

    import libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputSerch = document.querySelector('#search-box');
+
+inputSerch.addEventListener('input', inputValue())
+
+function inputValue() {
+   console.log(inputSerch.value);
+}
