15ce8589f314adf2d79c9e67c076b8681e6ef603
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-16 22:08:33 +0300

    correct index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,26 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
-import countriesListMarkup from './templates/countriesListMarkup.hbs'
-import countryInfoMarkup from './templates/countryInfoMarkup.hbs'
+import countriesListMarkup from './templates/countriesListMarkup.hbs';
+import countryInfoMarkup from './templates/countryInfoMarkup.hbs';
 
 const DEBOUNCE_DELAY = 300;
 const searchField = document.querySelector('#search-box');
 const countryInfo = document.querySelector('.country-info');
 const countriesList = document.querySelector('.country-list');
-searchField.value = ''
+searchField.value = '';
 
 function markupCleaner() {
-   countryInfo.innerHTML = '';
-   countriesList.innerHTML = '';
- }
+  countryInfo.innerHTML = '';
+  countriesList.innerHTML = '';
+}
 
 function countriesMarkup(countries) {
+  markupCleaner();
   if (countries.length > 1 && countries.length <= 10) {
-    markupCleaner();
-    const countriesListRender = countriesListMarkup(countries)
+    const countriesListRender = countriesListMarkup(countries);
     countriesList.insertAdjacentHTML('afterbegin', countriesListRender);
   } else if (countries.length === 1) {
-    markupCleaner();
     const countryInfoRedner = countryInfoMarkup(countries);
     countryInfo.insertAdjacentHTML('afterbegin', countryInfoRedner);
   } else if (countries.length > 10) {
@@ -31,18 +30,16 @@ function countriesMarkup(countries) {
 }
 
 function inputHandler(e) {
-  markupCleaner();
   const inputValue = e.target.value.trim();
-
-    if (inputValue.length === 0) {
-       return
-    }
-
+  if (inputValue.length === 0) {
+    return;
+  }
   fetchCountries(inputValue)
     .then(countries => {
       countriesMarkup(countries);
     })
     .catch(() => Notiflix.Notify.failure(`Oops, there is no country with that name.`));
+  markupCleaner();
 }
 
 searchField.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
